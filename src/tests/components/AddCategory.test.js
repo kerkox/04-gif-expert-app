b0e0f593a48e2c30f0461bcd232f1b2bf5ee8981
Paul Cortes <paulkerg@gmail.com>
2021-01-16 01:45:02 -0500

    Pruebas de AddCategory!

---
+++ src/tests/components/AddCategory.test.js
@@ -0,0 +1,47 @@
+import { shallow } from "enzyme"
+import '@testing-library/jest-dom';
+import { AddCategory } from "../../components/AddCategory"
+
+describe('Pruebas en <AddCategory/>', () => {
+
+    const setCategories = jest.fn();
+    let wrapper = shallow(<AddCategory setCategories={setCategories} />);
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+        wrapper = shallow(<AddCategory setCategories={setCategories} />);
+    })
+
+    test('debe de mostrarse correctamente', () => {
+        expect(wrapper).toMatchSnapshot();
+    })
+
+    test('debe de cambair la caja de texto', () => {
+        const input = wrapper.find('input');
+        const value = 'Hola Mundo';
+        input.simulate('change', { target: { value } })
+    })
+
+    test('No debe de postear la información con submit', () => {
+        wrapper.find('form').simulate('submit', {
+            preventDefault() { }
+        })
+
+        expect(setCategories).not.toHaveBeenCalled();
+    })
+
+    test('debe de llamar el setCategories y limpiar la caja de texto', () => {
+        const value = 'Hola Mundo!!!!';
+        const input = wrapper.find('input')
+        input.simulate('change', { target: { value } })
+        const form = wrapper.find('form');
+        form.simulate('submit', { preventDefault(){} });
+        expect(setCategories).toHaveBeenCalled();
+        expect(setCategories).toHaveBeenCalledWith( expect.any(Function));
+
+        expect(input.text()).toBe('')
+    })
+
+
+
+})
