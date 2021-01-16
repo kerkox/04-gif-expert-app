49b3b4772e6021bf83f324f16a21cb697c34b56f
Paul Cortes <paulkerg@gmail.com>
2021-01-15 21:02:38 -0500

    mejoras de GifGrid

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useFetchGifs } from '../hooks/useFetchGifs';
-// import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
