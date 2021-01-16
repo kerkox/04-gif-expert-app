19f78d041115a81f978805f55b6c66f92e74a810
Paul Cortes <paulkerg@gmail.com>
2021-01-16 02:30:17 -0500

    pruebas de hooks

--- src/tests/hooks/useFetchGifs.test.js
+++ src/tests/hooks/useFetchGifs.test.js
@@ -1,9 +1,25 @@
+import { renderHook } from '@testing-library/react-hooks'
 import { useFetchGifs } from "../../hooks/useFetchGifs"
 
 describe('Pruebas en el hook useFetchGifs', () => {
-    test('debe de retornar el estado inical', () => {
-        const { data: images, loading } = useFetchGifs('Mr Robot');
 
+    test('debe de retornar el estado inical', async () => {
+        const { result, waitForNextUpdate} = renderHook(() => useFetchGifs('Mr Robot'))
+        const {data, loading} = result.current;
+
+        await waitForNextUpdate();
+
+        expect( data).toEqual([]);
+        expect(loading).toBeTruthy();
     })
+
+    test('debe de retornar un arreglo de imgs y el loading en false', async () => {
+        const { result, waitForNextUpdate } = renderHook(() => useFetchGifs('Mr Robot'))
+        await waitForNextUpdate();
+        const { data, loading } = result.current;
+        expect(data.length).toBe(10);
+        expect(loading).toBeFalsy();
+    })
+    
     
 })
